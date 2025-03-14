17b7b793a8efee3112f4cf8ee6e54e1d96cd8bf7
RumenIliev <rumen.iliev50@gmail.com>
2025-03-14 21:10:03 +0200

    Update

---
+++ Course_Project/advanced_examples.py
@@ -0,0 +1,88 @@
+"""
+1.
+Read A;
+Read B;
+Read C;
+D := (A + B) * C;
+Write D;
+
+
+2.
+Read X;
+Y := X * X;
+Z := X + Y;
+Write Z;
+
+
+3.
+Read A;
+B := A + 5;
+C := B * 2;
+D := C - A;
+Write D;
+
+
+4.
+Read M;
+N := M + 5;
+P := N * 2;
+Q := P - N;
+R := Q DIV 3;
+Write R;
+
+
+5.
+Read A;
+Read B;
+L := A MOD B;
+Write L;
+
+
+6.
+Read A;
+Read B;
+Read C;
+X := A OR (B AND C);
+Write X;
+
+
+7.
+Read A;
+W := NOT A;
+Write W;
+
+
+8.
+Read A;
+Read B;
+C := (A * B) DIV 2;
+D := (A + B) * (C MOD 3);
+E := D DIV (B + 1);
+F := C * (E DIV 2);
+X := (A MOD 4) + F;
+Write X;
+
+
+9.
+Read A;
+Read B;
+Z := (A + 2) * (B - 3);
+W := Z * 4;
+P := (W + A) * (B + 5);
+Q := P * (Z - 6);
+X := (W + Q) DIV 2;
+Write X;
+
+
+10.
+Read X;
+Read Y;
+Z := X + Y;
+W := Z * 2;
+P := W + 7;
+Q := P DIV 3;
+R := Q - 4;
+S := R * X;
+T := Y + S;
+Write T;
+"""
